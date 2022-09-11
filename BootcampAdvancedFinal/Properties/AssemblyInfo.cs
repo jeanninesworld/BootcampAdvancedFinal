b51d78bd4cfb7ec4d5b53a6e30e19179744310db
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2022-09-11 13:00:49 -0400

    Add project files.

---
+++ BootcampAdvancedFinal/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BootcampAdvancedFinal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AgileThought")]
+[assembly: AssemblyProduct("BootcampAdvancedFinal")]
+[assembly: AssemblyCopyright("Copyright © AgileThought 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c79c3ab-d28f-452b-bd55-75a4fa15023a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
